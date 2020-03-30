d971e46be3b562671753ca809e9725b68883e6e3
gcuadrado <gcuadrado15@gmail.com>
2020-03-30 20:50:58 +0200

    Añadida clase para que se puedan recibir MultiPartForms y creado el primer Rest que recibe fotos y las almacena en disco

---
+++ src/main/java/rest/JAXRSMultiPart.java
@@ -0,0 +1,9 @@
+package rest;
+
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class JAXRSMultiPart extends MultiPartFeature {
+}
--- src/main/java/rest/RestPuntosInteres.java
+++ src/main/java/rest/RestPuntosInteres.java
@@ -2,12 +2,17 @@ package rest;
 
 import modelo.dto.PuntoInteresDtoGetDetalle;
 import modelo.dto.PuntoInteresDtoGetMaestro;
+import org.glassfish.jersey.media.multipart.BodyPartEntity;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import servicios.ServiciosPuntoInteres;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Path("puntos")
@@ -32,5 +37,20 @@ public class RestPuntosInteres {
         PuntoInteresDtoGetDetalle puntoInteresDtoGetDetalle = serviciosPuntoInteres.get(id);
         response = Response.ok(puntoInteresDtoGetDetalle).build();
         return response;
+    }
+
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response addPuntoInteres(FormDataMultiPart multiPart) throws IOException {
+
+      InputStream inputStream=  ((BodyPartEntity)multiPart.getField("image").getEntity()).getInputStream();
+        FileOutputStream fos=new FileOutputStream("C:\\Tools\\payara5\\glassfish\\domains\\domain1\\docroot\\uploads\\images.jpeg");
+        byte[] bytes = new byte[1024];
+        int read;
+        while ((read = inputStream.read(bytes)) != -1) {
+            fos.write(bytes, 0, read);
+        }
+       fos.close();
+        return Response.ok().build();
     }
 }
